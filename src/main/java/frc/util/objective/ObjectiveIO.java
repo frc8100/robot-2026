b6951230b534d797f03d2964c6e705c5b5b2fb09
zBlade <xshadowblade618@gmail.com>
2026-01-13 15:10:37 +0000

    wip(objective): add tracking of active hub

--- src/main/java/frc/util/objective/ObjectiveIO.java
+++ src/main/java/frc/util/objective/ObjectiveIO.java
@@ -1,10 +1,90 @@
 package frc.util.objective;
 
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.units.measure.MutTime;
+import edu.wpi.first.wpilibj.DriverStation;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ObjectiveIO {
+    /**
+     * The hub that is currently active.
+     */
+    public enum ActiveHub {
+        /**
+         * All hubs are active, during auto and endgame.
+         */
+        ALL,
+
+        /**
+         * The red alliance hub is active.
+         */
+        RED,
+
+        /**
+         * The blue alliance hub is active.
+         */
+        BLUE;
+
+        /**
+         * Converts a {@link DriverStation.Alliance} to an {@link ActiveHub}.
+         * @param alliance - The alliance to convert.
+         * @return The corresponding ActiveHub.
+         */
+        public static ActiveHub fromAlliance(DriverStation.Alliance alliance) {
+            switch (alliance) {
+                case Red:
+                    return RED;
+                case Blue:
+                    return BLUE;
+                default:
+                    return ALL;
+            }
+        }
+
+        /**
+         * Checks if the given hub is active for the given alliance.
+         * @param hub - The hub to check.
+         * @param alliance - The alliance to check.
+         * @return True if the hub is active for the alliance, false otherwise.
+         */
+        public static boolean isActive(ActiveHub hub, DriverStation.Alliance alliance) {
+            switch (hub) {
+                case ALL:
+                    return true;
+                case RED:
+                    return alliance.equals(DriverStation.Alliance.Red);
+                case BLUE:
+                    return alliance.equals(DriverStation.Alliance.Blue);
+                default:
+                    return false;
+            }
+        }
+
+        /**
+         * Checks if this hub is active for the given alliance.
+         * @param alliance - The alliance to check.
+         * @return True if this hub is active for the alliance, false otherwise.
+         */
+        public boolean isActive(DriverStation.Alliance alliance) {
+            return isActive(this, alliance);
+        }
+    }
+
     @AutoLog
-    public static class ObjectiveIOInputs {}
+    public static class ObjectiveIOInputs {
+
+        /**
+         * The hub that is currently active.
+         */
+        public ActiveHub activeHub = ActiveHub.ALL;
+
+        /**
+         * The time remaining in the current period.
+         * Counts down to zero. Should always be non-negative.
+         */
+        public MutTime timeUntilSwitch = Seconds.mutable(0.0);
+    }
 
     public default void updateInputs(ObjectiveIOInputs inputs) {}
 }
--- src/main/java/frc/util/objective/ObjectiveIODashboard.java
+++ src/main/java/frc/util/objective/ObjectiveIODashboard.java
@@ -1,7 +1,17 @@
 package frc.util.objective;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class ObjectiveIODashboard implements ObjectiveIO {
 
     @Override
-    public void updateInputs(ObjectiveIOInputs inputs) {}
+    public void updateInputs(ObjectiveIOInputs inputs) {
+        if (DriverStation.isAutonomous()) {
+            inputs.activeHub = ObjectiveIO.ActiveHub.ALL;
+        }
+
+        // todo: update from https://gist.github.com/OliverW1986/f5910996381601e32cf475bf261f456d
+        double timeUntilNextPeriodSeconds = DriverStation.getMatchTime();
+    }
 }
