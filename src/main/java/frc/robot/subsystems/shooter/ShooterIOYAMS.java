dc9b72b0479d8955fecfd537f0888bee2eebff2c
xShadowBlade <xshadowblade618@gmail.com>
2026-01-26 11:58:44 -0500

    feat(shooter): use iterative time of flight solver and time of flight lookup

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOYAMS.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOYAMS.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.shooter;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.CANIdConstants;
 import frc.util.WrappedSpark;
 import yams.mechanisms.velocity.FlyWheel;
@@ -10,7 +11,11 @@ public class ShooterIOYAMS implements ShooterIO {
 
     // Shoot motor
     protected final SparkMax shootMotor = new SparkMax(CANIdConstants.SHOOTER_MOTOR_ID, MotorType.kBrushless);
-    protected final WrappedSpark shootMotorWrapped = new WrappedSpark(shootMotor, ShooterConstants.shootMotorConfig);
+    protected final WrappedSpark shootMotorWrapped = new WrappedSpark(
+        shootMotor,
+        // TODO: not a good way to do this
+        ShooterConstants.shootMotorConfig.withSubsystem(new Subsystem() {})
+    );
 
     // Shooter Mechanism
     private FlyWheel shooter = new FlyWheel(ShooterConstants.shooterConfig.apply(shootMotorWrapped));
