457303390ef245fdda96cebeabbe18266d2c0749
zBlade <xshadowblade618@gmail.com>
2026-02-22 21:13:36 -0500

    feat(climb): run climb motors, add climb targets

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -1,15 +1,45 @@
 package frc.robot.subsystems.climb;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.CANIdConstants;
+import frc.robot.subsystems.CANIdAlert;
 import frc.util.statemachine.StateMachine;
 import frc.util.statemachine.StateMachineState;
+import org.littletonrobotics.junction.Logger;
 
 public class Climb extends SubsystemBase {
 
     public enum ClimbState {
+        /**
+         * The climb is idle and stowed.
+         * Can transition to {@link #DEPLOYING}.
+         */
         IDLE,
+
+        /**
+         * The climb is deploying from the stowed position to the "ready to climb" position.
+         */
         DEPLOYING,
+
+        /**
+         * The climb is fully deployed and ready to climb.
+         * Can transition to {@link #CLIMBING}.
+         */
+        DEPLOYED,
+
+        /**
+         * The climb is climbing.
+         */
         CLIMBING,
+
+        /**
+         * The climb is holding its position.
+         */
+        HOLD_CLIMB,
+
+        /**
+         * The climb is descending.
+         */
         DESCENDING,
     }
 
@@ -20,7 +50,13 @@ public class Climb extends SubsystemBase {
         .withDefaultState(new StateMachineState<>(ClimbState.IDLE, "Idle"))
         .withState(new StateMachineState<>(ClimbState.DEPLOYING, "Deploying").requirePreviousStateToBe(ClimbState.IDLE))
         .withState(
+            new StateMachineState<>(ClimbState.DEPLOYED, "Deployed").requirePreviousStateToBe(ClimbState.DEPLOYING)
+        )
+        .withState(
             new StateMachineState<>(ClimbState.CLIMBING, "Climbing").requirePreviousStateToBe(ClimbState.DEPLOYING)
+        )
+        .withState(
+            new StateMachineState<>(ClimbState.HOLD_CLIMB, "HoldClimb").requirePreviousStateToBe(ClimbState.CLIMBING)
         )
         .withState(
             new StateMachineState<>(ClimbState.DESCENDING, "Descending").requirePreviousStateToBe(ClimbState.CLIMBING)
@@ -29,7 +65,32 @@ public class Climb extends SubsystemBase {
     private final ClimbIO io;
     private final ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
 
+    // Alerts
+    private final CANIdAlert leftDisconnectedAlert = new CANIdAlert(
+        CANIdConstants.LEFT_CLIMB_MOTOR_ID,
+        "LeftClimbMotor"
+    );
+    private final CANIdAlert rightDisconnectedAlert = new CANIdAlert(
+        CANIdConstants.RIGHT_CLIMB_MOTOR_ID,
+        "RightClimbMotor"
+    );
+
     public Climb(ClimbIO io) {
         this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Climb", inputs);
+
+        // Update alerts
+        leftDisconnectedAlert.updateConnectionStatus(inputs.leftClimbMotorConnected);
+        rightDisconnectedAlert.updateConnectionStatus(inputs.rightClimbMotorConnected);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        io.simIterate();
     }
 }
--- src/main/java/frc/robot/subsystems/climb/ClimbIO.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIO.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 import frc.util.SubsystemIOUtil.SparkMotorControllerData;
 import org.littletonrobotics.junction.AutoLog;
 
@@ -13,4 +15,19 @@ public interface ClimbIO {
         public SparkMotorControllerData rightClimbMotorData = new SparkMotorControllerData();
         public boolean rightClimbMotorConnected = true;
     }
+
+    /** Updates the set of loggable inputs. */
+    public default void updateInputs(ClimbIOInputs inputs) {}
+
+    public default void runClimbDutyCycle(double output) {}
+
+    public default void setClimbEncoderPosition(Rotation2d position) {
+        setClimbEncoderPosition(position.getMeasure());
+    }
+
+    public default void setClimbEncoderPosition(Angle position) {}
+
+    public default void setClimbTarget(Angle targetPosition) {}
+
+    public default void simIterate() {}
 }
