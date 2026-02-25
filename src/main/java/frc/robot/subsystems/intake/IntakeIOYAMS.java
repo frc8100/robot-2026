7bba0f89fd84bf20f67fe6ee74c2d58460fcffbc
GraysonFRC <91495348+GraysonFRC@users.noreply.github.com>
2026-02-25 16:00:51 -0500

    feat(intake): implement applying and removing soft limits

--- src/main/java/frc/robot/subsystems/intake/IntakeIOYAMS.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOYAMS.java
@@ -1,12 +1,17 @@
 package frc.robot.subsystems.intake;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Pounds;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -35,18 +40,24 @@ public class IntakeIOYAMS implements IntakeIO {
     protected final SparkMax deployMotor = new SparkMax(CANIdConstants.DEPLOY_MOTOR_ID, MotorType.kBrushless);
     protected final WrappedSpark deployMotorWrapped = new WrappedSpark(deployMotor, IntakeConstants.deployMotorConfig);
 
-    // protected final Arm deployArm = new Arm(IntakeConstants.deployArmConfigFunction.apply(deployMotorWrapped));
+    protected final Arm deployArm = new Arm(IntakeConstants.deployArmConfigFunction.apply(deployMotorWrapped));
 
     @Override
     public void setDeploySetpoint(Angle setpoint) {
         // deployArm.setMechanismPositionSetpoint(setpoint);
 
-        deployMotorWrapped.setPosition(setpoint);
+        // TODO: current disabled for "safety"
+        // deployMotorWrapped.setPosition(setpoint);
     }
 
     @Override
     public void runIntake(double speed) {
         intakeMotorWrapped.setDutyCycle(speed);
+    }
+
+    @Override
+    public void runDeployVoltage(Voltage output) {
+        deployMotorWrapped.setVoltage(output);
     }
 
     @Override
@@ -61,12 +72,22 @@ public class IntakeIOYAMS implements IntakeIO {
 
     @Override
     public void removeSoftLimits() {
-        // TODO: implement
+        SparkMaxConfig config = (SparkMaxConfig) deployMotorWrapped.getMotorControllerConfig();
+
+        config.softLimit.forwardSoftLimitEnabled(false);
+        config.softLimit.reverseSoftLimitEnabled(false);
+
+        deployMotor.configureAsync(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
     }
 
     @Override
     public void applySoftLimits() {
-        // TODO: implement
+        SparkMaxConfig config = (SparkMaxConfig) deployMotorWrapped.getMotorControllerConfig();
+
+        config.softLimit.forwardSoftLimitEnabled(true);
+        config.softLimit.reverseSoftLimitEnabled(true);
+
+        deployMotor.configureAsync(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
     }
 
     @Override
