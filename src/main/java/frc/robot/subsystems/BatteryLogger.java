c8b05dbc878bbdfc3b51bdf7fcebbcedf41cb82f
zBlade <xshadowblade618@gmail.com>
2026-04-03 14:09:15 +0000

    wip: log battery currents

--- src/main/java/frc/robot/subsystems/BatteryLogger.java
+++ src/main/java/frc/robot/subsystems/BatteryLogger.java
@@ -1,16 +1,24 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.units.measure.MutCurrent;
 import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.CANIdConstants;
 import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
 
 public class BatteryLogger {
+
+    public static final int NUM_CHANNELS = 24;
 
     @AutoLog
     public static class BatteryLoggerIOInputs {
 
-        public double[] channelCurrents = new double[24];
+        public double[] channelCurrentsAmps = new double[NUM_CHANNELS];
+        public double totalCurrentAmps = 0.0;
     }
+
+    private final BatteryLoggerIOInputsAutoLogged inputs = new BatteryLoggerIOInputsAutoLogged();
 
     private final PowerDistribution powerDistribution = new PowerDistribution(
         CANIdConstants.POWER_DISTRIBUTION_ID,
@@ -18,4 +26,13 @@ public class BatteryLogger {
     );
 
     public BatteryLogger() {}
+
+    public void periodic() {
+        // TODO: IO implementation
+        // TODO: reduce allocation by reusing existing array
+        inputs.channelCurrentsAmps = powerDistribution.getAllCurrents();
+        inputs.totalCurrentAmps = powerDistribution.getTotalCurrent();
+
+        Logger.processInputs("BatteryLogger", inputs);
+    }
 }
